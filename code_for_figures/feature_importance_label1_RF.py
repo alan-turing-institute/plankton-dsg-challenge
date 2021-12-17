9acb973a6b1e17625c2424d876a39e2e1fb670d6
dsj976 <d.i.salvador-jasin@lboro.ac.uk>
2021-12-17 11:37:53 +0000

    Scripts to make figures for RF feature importance

---
+++ code_for_figures/feature_importance_label1_RF.py
@@ -0,0 +1,25 @@
+# see notebook random_forest_label1.ipynb
+
+# Creating a bar plot
+
+my_barplot = sns.barplot(x=feature_imp, y=feature_imp.index)
+
+fig = my_barplot.get_figure()
+
+plt.rcParams['figure.figsize'] = [6, 10]
+
+#plt.rcParams['figure.dpi'] = 100
+
+# Add labels to your graph
+
+plt.xlabel('Feature Importance Score', fontsize = 16)
+
+plt.ylabel('Features', fontsize = 16)
+
+plt.xticks(fontsize=12)
+
+plt.tight_layout()
+
+plt.show()
+
+fig.savefig('/output/report_figs/feature_importance_label1_RF.png')
